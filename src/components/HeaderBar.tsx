943157146991b7d4a40f1ca642b66e48469eeb93
joshi-hahahaha <joshi.ha7@gmail.com>
2025-02-23 11:36:24 +1100

    IMP: headerbar.

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCameraRetro } from "@fortawesome/free-solid-svg-icons";
 
-function HeaderBar() {
+type Props = {
+  text: string;
+};
+
+const HeaderBar = ({ text }: Props) => {
   return (
-    <div className="w-full h-20 flex justify-center items-center text-4xl text-white mb-4">
-      <div>{`Welcome to Joshi's Photobooth`}</div>
+    <div className="w-full h-20 px-8 flex space-x-6 items-center text-4xl text-black mb-4 bg-pastelWhite bg-opacity-70 rounded-t-2xl">
+      <FontAwesomeIcon icon={faCameraRetro} style={{ color: "#292929" }} />
+      <div>{text}</div>
     </div>
   );
-}
+};
 
 export default HeaderBar;
--- src/components/sections/StartSection.tsx
+++ src/components/sections/StartSection.tsx
@@ -1,5 +1,6 @@
 import { motion, Variants } from "motion/react";
 import React from "react";
+import HeaderBar from "../HeaderBar";
 
 type Props = {
   cardVariants: Variants;
@@ -15,11 +16,9 @@ const StartSection = ({ cardVariants, nextStep }: Props) => {
       exit="exit"
       variants={cardVariants}
       transition={{ duration: 0.5 }}
-      className="w-[40%] min-w-96 h-[70%] bg-white rounded-2xl flex flex-col justify-between drop-shadow-lg"
+      className="w-[40%] min-w-96 h-[70%] bg-pastelWhite bg-opacity-70 rounded-2xl flex flex-col justify-between drop-shadow-lg"
     >
-      <div className="w-full h-20 flex justify-center items-center text-4xl text-black">
-        {`Welcome to Joshi's Photobooth`}
-      </div>
+      <HeaderBar text="Joshi's Photobooth" />
       <div className="w-full h-20 flex justify-center items-center text-2xl text-black">
         <div>{`TODO: Information Section`}</div>
       </div>
